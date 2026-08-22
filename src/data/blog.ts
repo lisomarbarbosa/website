55abfca02135d3ffb2082ca945fbc41ce397ccdf
Lisomar Barbosa Adv <advlisomarbarbosa@gmail.com>
2026-08-22 16:04:56 -0300

    feat: create src/data/blog.ts required by generate-post workflow

---
+++ src/data/blog.ts
@@ -0,0 +1,13 @@
+export interface BlogPost {
+  slug: string;
+  title: string;
+  excerpt: string;
+  date: string;
+  readTime: string;
+  category: string;
+  image: string;
+  content: string;
+}
+
+export const blogPosts: BlogPost[] = [
+];
